89b2d232c07cf22ce8fa42aea675fa1b8beadbf3
Taras Lapytskyi <84970031+laptar@users.noreply.github.com>
2022-08-08 21:13:46 +0300

    start page style

--- src/components/StartPage/StartPage.jsx
+++ src/components/StartPage/StartPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import s from './StartPage.module.css';
 
 import { addNameXAction, addNameOAction } from 'redux/actions';
 
@@ -31,17 +32,40 @@ export const StartPage = ({ onStart }) => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Player"X"
-          <input name="X" value={playerX} onChange={handleChangeInp} />
+      <form onSubmit={handleSubmit} className={s.form}>
+        <label className={s.label}>
+          <span className={s.name}> Player X</span>
+          <input
+            className={s.input}
+            type="text"
+            autoComplete="off"
+            name="X"
+            value={playerX}
+            placeholder="Player X name"
+            onChange={handleChangeInp}
+          />
         </label>
-        <label>
-          Player"O"
-          <input name="O" value={playerO} onChange={handleChangeInp} />
+        <label className={s.label}>
+          <span className={s.name}> Player O</span>
+          <input
+            className={s.input}
+            type="text"
+            autoComplete="off"
+            name="O"
+            value={playerO}
+            placeholder="Player O name"
+            onChange={handleChangeInp}
+          />
         </label>
-        <button type="submit">START</button>
+        <button className={s.submit} type="submit">
+          START
+        </button>
       </form>
     </div>
   );
 };
+// className={s.input}
+//         type="text"
+//         autoComplete="off"
+//         autoFocus
+//         placeholder="Search movies"
